149bb7c8ab6d1a3f234206a171ff95595e1c2887
Nick Heiner <nth23@cornell.edu>
2017-07-11 18:57:09 -0400

    Basic graphql querying in place

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 #! /usr/bin/env node
 
 const logger = require('./logger');
-logger.info('hi');
+const githubGraphqlClient = require('github-graphql-client');
+
+const queryGithub = options => new Promise((resolve, reject) => {
+  githubGraphqlClient(options, (err, ...rest) => {
+    if (err) {
+      reject(err);
+    }
+    resolve(rest);
+  });
+});
+
+(async() => {
+  const queryResult = await queryGithub({
+    token: process.env.GITHUB_OAUTH_TOKEN,
+    query: `
+      query { 
+        repository(owner:"department-of-veterans-affairs", name:"caseflow") {
+          issues(last:5, states:CLOSED) {
+            edges {
+              node {
+                comments(last: 1) {
+                  nodes {
+                    author {
+                      login
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  });
+
+  logger.info({queryResult}, 'got query result');
+})();
