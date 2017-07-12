d30b3861994e8f90d08179f18787d51847558216
Nick Heiner <nth23@cornell.edu>
2017-07-11 23:48:10 -0400

    finding issues per person

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const logger = require('./logger');
 const githubGraphqlClient = require('github-graphql-client');
+const _ = require('lodash');
 
 const queryGithub = options => new Promise((resolve, reject) => {
   githubGraphqlClient(options, (err, ...rest) => {
@@ -16,13 +17,19 @@ const queryGithub = options => new Promise((resolve, reject) => {
   const queryResult = await queryGithub({
     token: process.env.GITHUB_OAUTH_TOKEN,
     query: `
-      query { 
-        repository(owner:"department-of-veterans-affairs", name:"caseflow") {
-          issues(last:5, states:CLOSED) {
+      {
+        repository(owner: "department-of-veterans-affairs", name: "caseflow") {
+          issues(last: 5, states: CLOSED, orderBy: {
+            field: UPDATED_AT
+            direction: ASC
+          }) {
             edges {
               node {
-                comments(last: 1) {
+                title
+                resourcePath
+                comments(first: 100) {
                   nodes {
+                    bodyText
                     author {
                       login
                     }
@@ -36,7 +43,27 @@ const queryGithub = options => new Promise((resolve, reject) => {
     `
   });
 
-  logger.info({queryResult}, 'got query result');
+  const commentsPassedForPerson = login => {
+    const issues = _.filter(
+      queryResult[0].data.repository.issues.edges, 
+      issueNode => _.some(
+        issueNode.node.comments.nodes, 
+        comment => _.includes(comment.bodyText, 'PASSED') && comment.author.login === login
+      )
+    );
+    
+    return {
+      issues: _.map(issues, issueNode => `https://github.com/${issueNode.node.resourcePath}`),
+      count: issues.length
+    };
+  };
+
+  const issuesPassedPerPerson = {
+    artem: commentsPassedForPerson('kierachell'),
+    alexis: commentsPassedForPerson('astewarttistatech')
+  };
+
+  logger.info({issuesPassedPerPerson});
 })();
 
 /*
@@ -46,6 +73,35 @@ const queryGithub = options => new Promise((resolve, reject) => {
       edges {
         node {
           title
+          timeline(last: 2) {
+            edges {
+              node {
+                ... on ClosedEvent {
+                  actor {
+                    login
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+*/
+
+/*
+{
+  repository(owner: "department-of-veterans-affairs", name: "caseflow") {
+    issues(last: 5, states: CLOSED, orderBy: {
+      field: UPDATED_AT,
+      direction: ASC
+    }) {
+      edges {
+        node {
+          title
+          resourcePath
           timeline(last: 2) {
             edges {
               node {
