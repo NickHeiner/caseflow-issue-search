32e4398b529f682bbb9dd3abbcc6c322797a2a9b
Nick Heiner <nth23@cornell.edu>
2017-07-12 00:02:40 -0400

    trying to grab all issues but it loops forever

--- index.js
+++ index.js
@@ -3,26 +3,39 @@
 const logger = require('./logger');
 const githubGraphqlClient = require('github-graphql-client');
 const _ = require('lodash');
+const moment = require('moment');
 
 const queryGithub = options => new Promise((resolve, reject) => {
   githubGraphqlClient(options, (err, ...rest) => {
     if (err) {
+      logger.error(err);
       reject(err);
     }
     resolve(rest);
   });
 });
 
+const query = graphQl => queryGithub({
+  token: process.env.GITHUB_OAUTH_TOKEN,
+  query: graphQl
+});
+
 (async() => {
-  const queryResult = await queryGithub({
-    token: process.env.GITHUB_OAUTH_TOKEN,
-    query: `
+  const foundIssues = [];
+  let startCursor = null;
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const queryResult = await query(`
       {
         repository(owner: "department-of-veterans-affairs", name: "caseflow") {
-          issues(last: 5, states: CLOSED, orderBy: {
+          issues(last: 100, states: CLOSED, before: ${startCursor ? `"${startCursor}"` : null} orderBy: {
             field: UPDATED_AT
             direction: ASC
           }) {
+            pageInfo {
+              startCursor
+            }
             edges {
               node {
                 title
@@ -40,8 +53,24 @@ const queryGithub = options => new Promise((resolve, reject) => {
           }
         }
       }
-    `
-  });
+    `);
+
+    const issuesAfterDateCutoff = _.takeWhile(
+      queryResult[0].data.repository.issues.edges,
+      edge => moment(edge.node.updatedAt).isAfter(moment('2017-05-01'))
+    );
+
+    foundIssues.push(...issuesAfterDateCutoff);
+
+    if (issuesAfterDateCutoff.length < queryResult[0].data.repository.issues.edges.length) {
+      break;
+    }
+
+    startCursor = queryResult[0].data.repository.issues.pageInfo.startCursor;
+    logger.info({startCursor}, 'Setting start cursor');
+  }
+
+  logger.info({foundIssuesCount: foundIssues.length}, 'Got issues');
 
   const commentsPassedForPerson = login => {
     const issues = _.filter(
